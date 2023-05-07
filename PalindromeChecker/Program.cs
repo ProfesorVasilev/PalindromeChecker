bc6b957f917d97b5b95173291b2dbd944f564087
ProfesorVasilev <vladigotaniq@gmail.com>
2023-05-07 16:05:28 +0300

    Add project files.

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,16 @@
+﻿do
+{
+    Console.WriteLine("type your word");
+    string str = Console.ReadLine();
+    char[] strArray = str.ToCharArray();
+    Array.Reverse(strArray);
+    string reversed = new string(strArray);
+    if (str == reversed)
+    {
+        Console.WriteLine("palindrome");
+    }
+    else
+    {
+        Console.WriteLine("not a palindrome");
+    }
+}while(true);
